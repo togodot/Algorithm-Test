c3da7096b8fdfa998817f7f45209c8dc3745069d
Jungwon <101785318+togodot@users.noreply.github.com>
2022-11-23 16:26:41 +0900

    Create 10989.py

---
+++ Python(baekjoon)/10989.py
@@ -0,0 +1,25 @@
+# 10989번: 수 정렬하기 3
+
+# 문제
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+# 
+# 입력
+# 첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+# 
+# 출력
+# 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+from sys import stdin
+def input():
+    return stdin.readline().rstrip()
+
+N = int(input())
+arr = [0] * 10000
+
+for _ in range(N):
+    n = int(input())
+    arr[n-1] += 1
+for i in range(10000):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i+1)
