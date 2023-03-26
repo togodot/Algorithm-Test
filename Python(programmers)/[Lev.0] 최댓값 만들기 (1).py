8e9a410ef54b3ae2adb8c25b8e0613ab679dd5c9
Jungwon <101785318+togodot@users.noreply.github.com>
2023-03-26 23:37:12 +0900

    Update [Lev.0] 최댓값 만들기 (1).py

--- Python(programmers)/[Lev.0] 최댓값 만들기 (1).py
+++ Python(programmers)/[Lev.0] 최댓값 만들기 (1).py
@@ -2,14 +2,14 @@
 
 # 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
 
-### max함수가 정렬보다 시간복잡도가 적을거라 생각해 작성한 코드
+### max함수가 정렬보다 시간복잡도가 적을거라 생각해 작성한 코드 >> max 함수의 시간복잡도 O(N)
 def solution(numbers):
     a = max(numbers)
     numbers.remove(a)
     b = max(numbers)
     return a*b
 
-### 정렬로 풀이한 코드
+### 정렬로 풀이한 코드 >> sort 함수의 시간복잡도 O(n log n)
 def solution(numbers):
     numbers.sort()
     return numbers[-2] * numbers[-1]
