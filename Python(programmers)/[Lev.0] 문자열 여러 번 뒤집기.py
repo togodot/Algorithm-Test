3a668dc72d3b758e16192e0f672da4e411c0fbe2
Jungwon <101785318+togodot@users.noreply.github.com>
2023-05-06 21:20:55 +0900

    Create [Lev.0] 문자열 여러 번 뒤집기.py

---
+++ Python(programmers)/[Lev.0] 문자열 여러 번 뒤집기.py
@@ -0,0 +1,17 @@
+### Lev.0 문자열 여러 번 뒤집기
+
+# 문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다. queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다.
+# my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+### 문자열 슬라이싱으로 순서를 뒤집어서 이어붙였다. 1점
+def solution(my_string, queries):
+    for query in queries:
+        s, e = query
+        my_string = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    return my_string
+
+### s, e를 for문에서 불러올 때 바로 불러올 수 있다는 점.
+def solution(my_string, queries):
+    for s, e in queries:
+        my_string = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    return my_string
