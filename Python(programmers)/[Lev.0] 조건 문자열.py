6f7fa9040c677b2c13ecd32129040edc2916ad9b
Jungwon <101785318+togodot@users.noreply.github.com>
2023-05-07 22:56:29 +0900

    Create [Lev.0] 조건 문자열.py

---
+++ Python(programmers)/[Lev.0] 조건 문자열.py
@@ -0,0 +1,22 @@
+### Lev.0 조건 문자열
+
+# 문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+# 
+# 두 수가 n과 m이라면
+# ">", "=" : n >= m
+# "<", "=" : n <= m
+# ">", "!" : n > m
+# "<", "!" : n < m
+# 두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다.
+# 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+
+### 
+def solution(ineq, eq, n, m):
+    if eq == '=':
+        return int(eval(str(n)+ineq+eq+str(m)))
+    else:
+        return int(eval(str(n)+ineq+str(m)))
+
+### if문으로 비교하는 대신 replace로 !에 대한 조건을 없애버리는 방법도 있다.
+def solution(ineq, eq, n, m):
+    return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))
