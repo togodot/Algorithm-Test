205eda9b39ea87bd75c2374400bb40879af16b32
Jungwon <101785318+togodot@users.noreply.github.com>
2023-06-05 00:23:06 +0900

    Update [Lev.0] 외계행성의 나이.py

--- Python(programmers)/[Lev.0] 외계행성의 나이.py
+++ Python(programmers)/[Lev.0] 외계행성의 나이.py
@@ -4,6 +4,6 @@
 # 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다. a는 0, b는 1, c는 2, ..., j는 9입니다.
 # 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
 
-### 문자->아스키코드 chr()함수 사용. 1점
+### 아스키코드->문자 변환하는 chr()함수 사용. 1점
 def solution(age):
     return ''.join(chr(int(n)+97) for n in str(age))
